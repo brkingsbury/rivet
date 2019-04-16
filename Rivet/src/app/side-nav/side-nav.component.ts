c901f6f0259c8c1ecba9bbeeeb69926606fa74cb
Joseph Wheaton <Joe.Wheaton@advicent.com>
2019-04-16 22:50:15 +0000

    Remove custom text input sidenav option and rearrange options while I'm in there Imported from Changeset 2130

--- Rivet/src/app/side-nav/side-nav.component.ts
+++ Rivet/src/app/side-nav/side-nav.component.ts
@@ -1,75 +1,65 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { RouterState } from '@angular/router';
-import { AppComponent } from '../app.component';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-side-nav',
-    templateUrl: './side-nav.component.html',
-    styleUrls: ['./side-nav.component.scss']
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
+  @Output() cursorStatus = new EventEmitter();
 
-    @Output() cursorStatus = new EventEmitter();
-
-    sideNavConfig = {
-        expandableObj: [
-            {
-                title: 'Components',
-                links: [
-                    { name: 'Checkboxes', path: '/components', section: 'checkbox' },
-                    { name: 'Datepicker', path: '/components', section: 'datepicker' },
-                    { name: 'Dialog', path: '/components', section: 'dialog' },
-                    { name: 'Dropdowns', path: '/components', section: 'dropdown' },
-                    { name: 'Event Picker', path: '/components', section: 'eventPicker'},
-                    { name: 'Inputs', path: '/components', section: 'input'},
-                    { name: 'Radio Buttons', path: '/components', section: 'radio' },
-                    { name: 'Tables', path: '/components', section: 'table' },
-                    { name: 'Tabs', path: '/components', section: 'tab' },
-                    { name: 'Text Inputs', path: '/components', section: 'input' },
-                    { name: 'Buttons', path: '/components', section: 'button' },
-                    { name: 'Expansion Panel', path: '/components', section: 'expansion' },
-                    { name: 'Loading', path: '/components', section: 'loading' },
-                    { name: 'Navigation', path: '/components', section: 'nav' },
-                    { name: 'Text Areas', path: '/components', section: 'textArea' },
-                    { name: 'Custom Text Inputs', path: '/components', section: 'rvtInput' }
-                ]
-            },
-            {
-                title: 'Style',
-                links: [
-                    { name: 'Icons', path: '/style', section: 'icon' },
-                    { name: 'Font Styles', path: '/style', section: 'font' },
-                    { name: 'Color Variables', path: '/style', section: 'color' }
-                ]
-            },
-            {
-                title: 'Patterns',
-                links: [
-                    { name: 'Accessibility', path: '/patterns', section: '' }
-                ]
-            },
-            {
-                title: 'Changelog',
-                links: [
-                    { name: 'Roadmap', path: '/changelog', section: 'roadmap' },
-                    { name: 'Versions', path: '/changelog', section: 'version' }
-                ]
-            }
+  sideNavConfig = {
+    expandableObj: [
+      {
+        title: 'Components',
+        links: [
+          { name: 'Checkboxes', path: '/components', section: 'checkbox' },
+          { name: 'Datepicker', path: '/components', section: 'datepicker' },
+          { name: 'Dialog', path: '/components', section: 'dialog' },
+          { name: 'Dropdowns', path: '/components', section: 'dropdown' },
+          { name: 'Event Picker', path: '/components', section: 'eventPicker' },
+          { name: 'Inputs', path: '/components', section: 'input' },
+          { name: 'Radio Buttons', path: '/components', section: 'radio' },
+          { name: 'Tables', path: '/components', section: 'table' },
+          { name: 'Tabs', path: '/components', section: 'tab' },
+          { name: 'Text Areas', path: '/components', section: 'textArea' },
+          { name: 'Buttons', path: '/components', section: 'button' },
+          { name: 'Expansion Panel', path: '/components', section: 'expansion' },
+          { name: 'Loading', path: '/components', section: 'loading' },
+          { name: 'Navigation', path: '/components', section: 'nav' }
         ]
-    };
+      },
+      {
+        title: 'Style',
+        links: [
+          { name: 'Icons', path: '/style', section: 'icon' },
+          { name: 'Font Styles', path: '/style', section: 'font' },
+          { name: 'Color Variables', path: '/style', section: 'color' }
+        ]
+      },
+      {
+        title: 'Patterns',
+        links: [{ name: 'Accessibility', path: '/patterns', section: '' }]
+      },
+      {
+        title: 'Changelog',
+        links: [
+          { name: 'Roadmap', path: '/changelog', section: 'roadmap' },
+          { name: 'Versions', path: '/changelog', section: 'version' }
+        ]
+      }
+    ]
+  };
 
-    expand(section) {
-        section.expanded = !section.expanded;
-    }
+  expand(section) {
+    section.expanded = !section.expanded;
+  }
 
-    toggleCursor() {
-        this.cursorStatus.emit();
-    }
+  toggleCursor() {
+    this.cursorStatus.emit();
+  }
 
+  constructor() {}
 
-    constructor() { }
-
-    ngOnInit() {
-    }
-
+  ngOnInit() {}
 }
