dfee0f156267905a18a1f878dcbc20b70d01783e
Kingsbury, Brian <Brian.Kingsbury@advicent.com>
2020-03-02 13:11:18 +0000

    Merged PR 2969: Add rivet-tile to Rivet package
    This is the base component for the rvt-tile. It is just the container with included functionality for the tile (selected, edit, and delete). Content is just passed in with ng-content for now. I worked with Metodi and Ender on making sure this works for their use cases in Essentials

--- projects/rivet-style/src/lib/rivet-style.module.ts
+++ projects/rivet-style/src/lib/rivet-style.module.ts
@@ -1,47 +1,50 @@
 import { CommonModule, registerLocaleData } from '@angular/common';
 import localeFrCA from '@angular/common/locales/fr-CA';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import 'hammerjs';
 import { MaterialComponentModule } from './material-component-module';
-import { RivetExpansionPanelComponent } from './rivet-expansion-panel/expansion-panel.component';
-import { RivetInputComponent } from './rivet-input/rivet-input.component';
-import { RivetHelperComponent } from './rivet-input-helper/rivet-input-helper.component';
-import { RivetLabelComponent } from './rivet-input-label/rivet-input-label.component';
-import { RivetStyleComponent } from './rivet-style.component';
-import { RvtTooltipDirective } from './rivet-tooltip/rivet-tooltip.directive';
-import { RivetMiniExpansionPanelComponent } from './rivet-mini-expansion-panel/rivet-mini-expansion-panel.component';
 import { RivetAccordionGroupComponent } from './rivet-accordion-group/rivet-accordion-group.component';
 import { RivetDrawerComponent } from './rivet-drawer/rivet-drawer.component';
+import { RivetExpansionPanelComponent } from './rivet-expansion-panel/expansion-panel.component';
+import { RivetHelperComponent } from './rivet-input-helper/rivet-input-helper.component';
+import { RivetLabelComponent } from './rivet-input-label/rivet-input-label.component';
+import { RivetInputComponent } from './rivet-input/rivet-input.component';
+import { RivetMiniExpansionPanelComponent } from './rivet-mini-expansion-panel/rivet-mini-expansion-panel.component';
+import { RivetStyleComponent } from './rivet-style.component';
+import { RivetTileComponent } from './rivet-tile/rivet-tile.component';
 import { RivetSlideToggleComponent } from './rivet-toggle/rivet-slide-toggle.component';
+import { RvtTooltipDirective } from './rivet-tooltip/rivet-tooltip.directive';
 
 registerLocaleData(localeFrCA, 'fr-CA');
 
 @NgModule({
-  imports: [CommonModule, MaterialComponentModule, FormsModule, ReactiveFormsModule],
-  declarations: [
-    RivetStyleComponent,
-    RivetExpansionPanelComponent,
-    RivetInputComponent,
-    RivetHelperComponent,
-    RivetLabelComponent,
-    RvtTooltipDirective,
-    RivetMiniExpansionPanelComponent,
-    RivetAccordionGroupComponent,
-    RivetDrawerComponent,
-    RivetSlideToggleComponent
-  ],
-  exports: [
-    RivetStyleComponent,
-    RivetExpansionPanelComponent,
-    RivetInputComponent,
-    RivetHelperComponent,
-    RivetLabelComponent,
-    RvtTooltipDirective,
-    RivetMiniExpansionPanelComponent,
-    RivetAccordionGroupComponent,
-    RivetDrawerComponent,
-    RivetSlideToggleComponent
-  ]
+    imports: [CommonModule, MaterialComponentModule, FormsModule, ReactiveFormsModule],
+    declarations: [
+        RivetStyleComponent,
+        RivetExpansionPanelComponent,
+        RivetInputComponent,
+        RivetHelperComponent,
+        RivetLabelComponent,
+        RvtTooltipDirective,
+        RivetMiniExpansionPanelComponent,
+        RivetAccordionGroupComponent,
+        RivetDrawerComponent,
+        RivetSlideToggleComponent,
+        RivetTileComponent
+    ],
+    exports: [
+        RivetStyleComponent,
+        RivetExpansionPanelComponent,
+        RivetInputComponent,
+        RivetHelperComponent,
+        RivetLabelComponent,
+        RvtTooltipDirective,
+        RivetMiniExpansionPanelComponent,
+        RivetAccordionGroupComponent,
+        RivetDrawerComponent,
+        RivetSlideToggleComponent,
+        RivetTileComponent
+    ]
 })
 export class RivetStyleModule { }
---
+++ projects/rivet-style/src/lib/rivet-tile/rivet-tile.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+    selector: 'rvt-tile',
+    templateUrl: './rivet-tile.component.html',
+    styleUrls: ['./rivet-tile.component.scss']
+})
+export class RivetTileComponent implements OnInit {
+
+    @Output() tileSelect = new EventEmitter();
+    @Output() tileEdit = new EventEmitter();
+    @Output() tileDelete = new EventEmitter();
+    @Input() config = {
+        isSelected: false,
+        deleteText: 'Are you sure you want to delete?',
+        cancelBtnText: 'Cancel',
+        deleteBtnText: 'Delete'
+    };
+
+    deleting = false;
+
+    editTile() {
+        this.tileEdit.emit();
+    }
+
+    selectTile() {
+        this.tileSelect.emit();
+    }
+
+    deleteTile() {
+        this.deleting = true;
+    }
+
+    confirmDelete() {
+        this.deleting = false;
+        this.tileDelete.emit();
+    }
+
+    cancelDelete() {
+        this.deleting = false;
+    }
+
+    constructor() { }
+
+    ngOnInit(): void { }
+
+}
