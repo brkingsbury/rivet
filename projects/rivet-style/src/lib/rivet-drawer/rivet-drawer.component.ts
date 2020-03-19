2b68482d447a82aeb4799532e21e13aba3945ba7
Rusch, John <John.Rusch@advicent.com>
2020-03-19 15:27:58 -0500

    Setup, applied to drawer

--- projects/rivet-style/src/lib/rivet-drawer/rivet-drawer.component.ts
+++ projects/rivet-style/src/lib/rivet-drawer/rivet-drawer.component.ts
@@ -1,41 +1,67 @@
-import { Component, Input, Output, EventEmitter, SimpleChanges, OnChanges, ViewEncapsulation } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
+
+const animations: any = [
+    trigger(
+        'fadeTransition',
+        [
+            transition(
+                ':enter',
+                [
+                    style({ opacity: 0 }),
+                    animate('400ms ease-in-out',
+                        style({ opacity: 1 }))
+                ]
+            ),
+            transition(
+                ':leave',
+                [
+                    style({ opacity: 1 }),
+                    animate('400ms ease-in-out',
+                        style({ opacity: 0 }))
+                ]
+            )
+        ]
+    )
+];
 
 @Component({
-  selector: 'rvt-drawer',
-  templateUrl: './rivet-drawer.component.html',
-  styleUrls: ['./rivet-drawer.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'rvt-drawer',
+    templateUrl: './rivet-drawer.component.html',
+    styleUrls: ['./rivet-drawer.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: animations
 })
 export class RivetDrawerComponent implements OnChanges {
-  @Input() drawerOpen: boolean;
+    @Input() drawerOpen: boolean;
 
-  @Output() public drawerCloseEvent = new EventEmitter<null>();
+    @Output() public drawerCloseEvent = new EventEmitter<null>();
 
-  public get panelOpen(): boolean {
-    return this.drawerOpen;
-  }
-
-  public toggleDrawer = function (): void {
-    this.drawerOpen ? this.closeDrawer() : this.openDrawer();
-  };
-
-  public closeDrawer = function (): void {
-    this.drawerCloseEvent.emit();
-    this.drawerOpen = false;
-  };
-
-  public openDrawer = function (): void {
-    this.drawerOpen = true;
-  };
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.drawerOpen) {
-      if (changes.drawerOpen.currentValue === true) {
-        document.documentElement.style.overflowY = 'hidden';
-        this.drawerCloseEvent.emit();
-      } else if (changes.drawerOpen.currentValue === false && changes.drawerOpen.previousValue !== undefined) {
-        document.documentElement.style.overflowY = 'auto';
-      }
+    public get panelOpen(): boolean {
+        return this.drawerOpen;
     }
-  }
+
+    public toggleDrawer = function (): void {
+        this.drawerOpen ? this.closeDrawer() : this.openDrawer();
+    };
+
+    public closeDrawer = function (): void {
+        this.drawerCloseEvent.emit();
+        this.drawerOpen = false;
+    };
+
+    public openDrawer = function (): void {
+        this.drawerOpen = true;
+    };
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.drawerOpen) {
+            if (changes.drawerOpen.currentValue === true) {
+                document.documentElement.style.overflowY = 'hidden';
+                this.drawerCloseEvent.emit();
+            } else if (changes.drawerOpen.currentValue === false && changes.drawerOpen.previousValue !== undefined) {
+                document.documentElement.style.overflowY = 'auto';
+            }
+        }
+    }
 }
