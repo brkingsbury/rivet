34371598b6d4f196723fceeb6d09b0e3b656795e
Joseph Wheaton <Joe.Wheaton@advicent.com>
2020-07-29 15:35:22 +0000

    Merged PR 5845: Update Common.Npm.Rivet to Angular 10
    Upgrade Rivet from Angular 9 to Angular 10

--- projects/rivet-style/src/lib/rivet-style.module.ts
+++ projects/rivet-style/src/lib/rivet-style.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import 'hammerjs';
 import { MaterialComponentModule } from './material-component-module';
 import { RivetAccordionGroupComponent } from './rivet-accordion-group/rivet-accordion-group.component';
 import { RivetDrawerComponent } from './rivet-drawer/rivet-drawer.component';
@@ -50,7 +49,7 @@ import { RvtTooltipDirective } from './rivet-tooltip/rivet-tooltip.directive';
 
 })
 export class RivetStyleModule {
-    static forRoot(): ModuleWithProviders {
+    static forRoot(): ModuleWithProviders<RivetStyleModule> {
         return {
             ngModule: RivetStyleModule,
             providers: [RivetThemingService]
